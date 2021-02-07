58547dcd58c6bd1ad7a95392e0e3b283c90fb61c
WLBF <lbf1353@live.com>
2021-02-07 22:10:58 +0800

    percolator passed

--- percolator/src/client.rs
+++ percolator/src/client.rs
@@ -1,6 +1,10 @@
 use labrpc::*;
 
+use crate::msg;
 use crate::service::{TSOClient, TransactionClient};
+use std::sync::mpsc::channel;
+use std::thread::sleep;
+use std::time::Duration;
 
 // BACKOFF_TIME_MS is the wait time before retrying to send the request.
 // It should be exponential growth. e.g.
@@ -19,42 +23,201 @@ const RETRY_TIMES: usize = 3;
 #[derive(Clone)]
 pub struct Client {
     // Your definitions here.
+    tso_client: TSOClient,
+    txn_client: TransactionClient,
+    start_ts: u64,
+    writes: Vec<(Vec<u8>, Vec<u8>)>,
 }
 
 impl Client {
     /// Creates a new Client.
     pub fn new(tso_client: TSOClient, txn_client: TransactionClient) -> Client {
         // Your code here.
-        Client {}
+        Client {
+            tso_client,
+            txn_client,
+            start_ts: 0,
+            writes: vec![],
+        }
     }
 
     /// Gets a timestamp from a TSO.
     pub fn get_timestamp(&self) -> Result<u64> {
         // Your code here.
-        unimplemented!()
+        let mut i = 1;
+        let mut mul = 1;
+
+        loop {
+            let args = msg::TimestampRequest {};
+            let cli = self.tso_client.clone();
+            let (tx, rx) = channel();
+            self.tso_client.spawn(async move {
+                let res = cli.get_timestamp(&args).await;
+                // ignore tx send error
+                tx.send(res).unwrap_or(());
+            });
+
+            let res = rx.recv().expect("call get timestamp rx");
+
+            if i < RETRY_TIMES && res.is_err() {
+                sleep(Duration::from_millis(mul * BACKOFF_TIME_MS));
+                i += 1;
+                mul *= 2;
+                continue;
+            }
+
+            break res.map(|reply| reply.start_ts);
+        }
     }
 
     /// Begins a new transaction.
     pub fn begin(&mut self) {
-        // Your code here.
-        unimplemented!()
+        self.start_ts = self.get_timestamp().expect("get timestamp failed");
+        self.writes.clear();
     }
 
     /// Gets the value for a given key.
     pub fn get(&self, key: Vec<u8>) -> Result<Vec<u8>> {
-        // Your code here.
-        unimplemented!()
+        let mut i = 1;
+        let mut mul = 1;
+
+        loop {
+            let args = msg::GetRequest {
+                key: key.clone(),
+                start_ts: self.start_ts,
+            };
+
+            let cli = self.txn_client.clone();
+            let (tx, rx) = channel();
+            self.txn_client.spawn(async move {
+                let res = cli.get(&args).await;
+                // ignore tx send error
+                tx.send(res).unwrap_or(());
+            });
+
+            let res = rx.recv().expect("call get rx");
+
+            if i < RETRY_TIMES && res.is_err() {
+                sleep(Duration::from_millis(mul * BACKOFF_TIME_MS));
+                i += 1;
+                mul *= 2;
+                continue;
+            }
+
+            break res.map(|reply| reply.value);
+        }
     }
 
     /// Sets keys in a buffer until commit time.
     pub fn set(&mut self, key: Vec<u8>, value: Vec<u8>) {
-        // Your code here.
-        unimplemented!()
+        self.writes.push((key, value));
+    }
+
+    fn call_prewrite(&self, w: (Vec<u8>, Vec<u8>), primary: (Vec<u8>, Vec<u8>)) -> Result<bool> {
+        let mut i = 1;
+        let mut mul = 1;
+        loop {
+            let (w_key, w_val) = w.clone();
+            let (p_key, p_val) = primary.clone();
+
+            let args = msg::PrewriteRequest {
+                write: Some(msg::Write {
+                    key: w_key,
+                    value: w_val,
+                }),
+                primary: Some(msg::Write {
+                    key: p_key,
+                    value: p_val,
+                }),
+                start_ts: self.start_ts,
+            };
+
+            let cli = self.txn_client.clone();
+            let (tx, rx) = channel();
+            self.txn_client.spawn(async move {
+                let res = cli.prewrite(&args).await;
+                // ignore tx send error
+                tx.send(res).unwrap_or(());
+            });
+
+            let res = rx.recv().expect("call prewrite rx");
+
+            if i < RETRY_TIMES && res.is_err() {
+                sleep(Duration::from_millis(mul * BACKOFF_TIME_MS));
+                i += 1;
+                mul *= 2;
+                continue;
+            }
+
+            break res.map(|reply| reply.success);
+        }
+    }
+
+    fn call_commit(&self, commit_ts: u64, is_primary: bool, w: (Vec<u8>, Vec<u8>)) -> Result<bool> {
+        let mut i = 1;
+        let mut mul = 1;
+        loop {
+            let (w_key, w_val) = w.clone();
+            let args = msg::CommitRequest {
+                start_ts: self.start_ts,
+                commit_ts,
+                is_primary,
+                write: Some(msg::Write {
+                    key: w_key,
+                    value: w_val,
+                }),
+            };
+            let cli = self.txn_client.clone();
+            let (tx, rx) = channel();
+            self.txn_client.spawn(async move {
+                let res = cli.commit(&args).await;
+                // ignore tx send error
+                tx.send(res).unwrap_or(());
+            });
+
+            let res = rx.recv().expect("call commit rx");
+
+            if i < RETRY_TIMES && res.is_err() {
+                sleep(Duration::from_millis(mul * BACKOFF_TIME_MS));
+                i += 1;
+                mul *= 2;
+                continue;
+            }
+
+            break res.map(|reply| reply.success);
+        }
     }
 
     /// Commits a transaction.
     pub fn commit(&self) -> Result<bool> {
-        // Your code here.
-        unimplemented!()
+        let mut it = self.writes.iter();
+        let primary = it.next().expect("none write");
+        let secondary = it
+            .map(|x| x.to_owned())
+            .collect::<Vec<(Vec<u8>, Vec<u8>)>>();
+
+        if !self.call_prewrite(primary.clone(), primary.clone())? {
+            return Ok(false);
+        }
+
+        for w in secondary.iter() {
+            if !self.call_prewrite(w.clone(), primary.clone())? {
+                return Ok(false);
+            }
+        }
+
+        // Commit primary first.
+        let commit_ts = self.get_timestamp()?;
+        let res = self.call_commit(commit_ts, true, primary.clone());
+        if res == Err(Error::Other("reqhook".to_owned())) || !res? {
+            return Ok(false);
+        }
+
+        // Second phase: write out write records for secondary cells.
+        for w in secondary.iter() {
+            let _ = self.call_commit(commit_ts, false, w.clone());
+        }
+
+        Ok(true)
     }
 }
--- percolator/src/server.rs
+++ percolator/src/server.rs
@@ -1,6 +1,8 @@
 use std::collections::BTreeMap;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 use crate::msg::*;
 use crate::service::*;
@@ -20,12 +22,21 @@ pub struct TimestampOracle {
 impl timestamp::Service for TimestampOracle {
     // example get_timestamp RPC handler.
     async fn get_timestamp(&self, _: TimestampRequest) -> labrpc::Result<TimestampResponse> {
-        // Your code here.
-        unimplemented!()
+        Ok(TimestampResponse {
+            start_ts: current_time(),
+        })
     }
 }
 
-// Key is a tuple (raw key, timestamp).
+fn current_time() -> u64 {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("time went backwards");
+    since_the_epoch.as_nanos() as u64
+}
+
+// Key is a tuple (raw.key.clone(), timestamp).
 pub type Key = (Vec<u8>, u64);
 
 #[derive(Clone, PartialEq)]
@@ -37,6 +48,7 @@ pub enum Value {
 #[derive(Debug, Clone)]
 pub struct Write(Vec<u8>, Vec<u8>);
 
+#[derive(Debug, Clone)]
 pub enum Column {
     Write,
     Data,
@@ -64,21 +76,42 @@ impl KvTable {
         ts_end_inclusive: Option<u64>,
     ) -> Option<(&Key, &Value)> {
         // Your code here.
-        unimplemented!()
+
+        let col = match column {
+            Column::Write => &self.write,
+            Column::Data => &self.data,
+            Column::Lock => &self.lock,
+        };
+
+        let ts_start = ts_start_inclusive.unwrap_or(0);
+        let ts_end = ts_end_inclusive.unwrap_or(u64::MAX);
+
+        col.range((key.clone(), ts_start)..=(key, ts_end)).last()
     }
 
     // Writes a record to a specified column in MemoryStorage.
     #[inline]
     fn write(&mut self, key: Vec<u8>, column: Column, ts: u64, value: Value) {
         // Your code here.
-        unimplemented!()
+        let col = match column {
+            Column::Write => &mut self.write,
+            Column::Data => &mut self.data,
+            Column::Lock => &mut self.lock,
+        };
+
+        col.insert((key, ts), value);
     }
 
     #[inline]
     // Erases a record from a specified column in MemoryStorage.
     fn erase(&mut self, key: Vec<u8>, column: Column, commit_ts: u64) {
         // Your code here.
-        unimplemented!()
+        let col = match column {
+            Column::Write => &mut self.write,
+            Column::Data => &mut self.data,
+            Column::Lock => &mut self.lock,
+        };
+        col.remove(&(key, commit_ts));
     }
 }
 
@@ -93,26 +126,169 @@ pub struct MemoryStorage {
 impl transaction::Service for MemoryStorage {
     // example get RPC handler.
     async fn get(&self, req: GetRequest) -> labrpc::Result<GetResponse> {
-        // Your code here.
-        unimplemented!()
+        loop {
+            let kv = self.data.lock().unwrap();
+            // Check for locks that signal concurrent writes.
+            let key = req.key.clone();
+            let start_ts = req.start_ts;
+
+            if kv
+                .read(key.clone(), Column::Lock, None, Some(start_ts))
+                .is_some()
+            {
+                // There is a pending lock; try to clean it and wait
+                std::mem::drop(kv);
+                self.back_off_maybe_clean_up_lock(start_ts, key);
+                continue;
+            }
+
+            // Find the latest write below our start timestamp
+            let opt = kv.read(key.clone(), Column::Write, None, Some(start_ts));
+
+            if opt.is_none() {
+                return Ok(GetResponse { value: vec![] });
+            }
+
+            let data_ts = match opt {
+                Some((_, Value::Timestamp(ts))) => ts.to_owned(),
+                _ => unreachable!(),
+            };
+
+            let (_, val) = kv
+                .read(key, Column::Data, Some(data_ts), Some(data_ts))
+                .expect("missing data");
+            let value = match val {
+                Value::Vector(v) => v.to_owned(),
+                _ => unreachable!(),
+            };
+
+            break Ok(GetResponse { value });
+        }
     }
 
     // example prewrite RPC handler.
     async fn prewrite(&self, req: PrewriteRequest) -> labrpc::Result<PrewriteResponse> {
-        // Your code here.
-        unimplemented!()
+        let w = req.write.unwrap();
+        let primary = req.primary.unwrap();
+        let start_ts = req.start_ts;
+        let mut kv = self.data.lock().unwrap();
+
+        // Abort on writes after our start timestamp ...
+        if kv
+            .read(w.key.clone(), Column::Write, Some(start_ts), None)
+            .is_some()
+        {
+            return Ok(PrewriteResponse { success: false });
+        }
+        // ... or locks at any timestamp.
+        if kv.read(w.key.clone(), Column::Lock, None, None).is_some() {
+            return Ok(PrewriteResponse { success: false });
+        }
+
+        kv.write(
+            w.key.clone(),
+            Column::Data,
+            start_ts,
+            Value::Vector(w.value),
+        );
+        // The primary’s location.
+        kv.write(w.key, Column::Lock, start_ts, Value::Vector(primary.key));
+
+        Ok(PrewriteResponse { success: true })
     }
 
     // example commit RPC handler.
     async fn commit(&self, req: CommitRequest) -> labrpc::Result<CommitResponse> {
-        // Your code here.
-        unimplemented!()
+        let w = req.write.unwrap();
+        let start_ts = req.start_ts;
+        let commit_ts = req.commit_ts;
+        let is_primary = req.is_primary;
+        let mut kv = self.data.lock().unwrap();
+        if is_primary
+            && kv
+                .read(w.key.clone(), Column::Lock, Some(start_ts), Some(start_ts))
+                .is_none()
+        {
+            // aborted while working
+            return Ok(CommitResponse { success: false });
+        }
+
+        // write out write records for secondary cells.
+
+        // Pointer to data written at start ts
+        kv.write(
+            w.key.clone(),
+            Column::Write,
+            commit_ts,
+            Value::Timestamp(start_ts),
+        );
+        // commit point
+        kv.erase(w.key, Column::Lock, start_ts);
+
+        Ok(CommitResponse { success: true })
     }
 }
 
 impl MemoryStorage {
     fn back_off_maybe_clean_up_lock(&self, start_ts: u64, key: Vec<u8>) {
-        // Your code here.
-        unimplemented!()
+        let mut kv = self.data.lock().unwrap();
+        let opt = kv.read(key.clone(), Column::Lock, None, Some(start_ts));
+        if opt.is_none() {
+            return;
+        }
+
+        let (lock_ts, primary_key) = match opt {
+            Some(((_, ts), Value::Vector(p_key))) => (ts.to_owned(), p_key.to_owned()),
+            _ => unreachable!(),
+        };
+
+        // back off if the key's lifetime not exceeds TTL.
+        if current_time() - lock_ts < TTL {
+            return;
+        }
+
+        // if key itself is primary and lock is present, erase the lock and data.
+        if primary_key == key {
+            kv.erase(primary_key.clone(), Column::Lock, lock_ts);
+            kv.erase(primary_key, Column::Data, lock_ts);
+            return;
+        }
+
+        // if primary lock is present, erase both primary and current lock and data.
+        // transaction that encounters the lock should rollback.
+        if kv
+            .read(
+                primary_key.clone(),
+                Column::Lock,
+                Some(lock_ts),
+                Some(lock_ts),
+            )
+            .is_some()
+        {
+            kv.erase(primary_key.clone(), Column::Lock, lock_ts);
+            kv.erase(primary_key, Column::Data, lock_ts);
+            kv.erase(key.clone(), Column::Lock, lock_ts);
+            kv.erase(key, Column::Data, lock_ts);
+            return;
+        }
+
+        // if primary lock is disappeared, check data @ primary.lock_ts to decide roll forward or roll back.
+        if kv
+            .read(primary_key, Column::Data, Some(lock_ts), Some(lock_ts))
+            .is_some()
+        {
+            // roll forward
+            kv.write(
+                key.clone(),
+                Column::Write,
+                lock_ts,
+                Value::Timestamp(lock_ts),
+            );
+            kv.erase(key, Column::Lock, lock_ts);
+        } else {
+            // roll back
+            kv.erase(key.clone(), Column::Lock, lock_ts);
+            kv.erase(key, Column::Data, lock_ts);
+        }
     }
 }
